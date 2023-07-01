6be3453617ed796f7040eab4a909355685b4492b
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-07-01 22:17:52 +0800

    Update c.py

--- c.py
+++ c.py
@@ -3,3 +3,5 @@ UUID=${UUID:-'xxxxxxxxxxxx'}
 VMPATH=${VMPATH:-'xxxxxxxxxx0'}
 VLPATH=${VLPATH:-'xxxxxxxxxxxxx0'}
 TOK=${TOK:-'eyJhIjoiNTRxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxRNCJ9'}
+NEZHA_SERVER='x'x'x'x'x'x'x'x'
+NEZHA_KEY='xxxxxxxxx'
