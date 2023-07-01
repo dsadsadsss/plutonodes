3c3745f6bdc6f13629fc98f3e92a6bea80a8d60a
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-07-01 22:21:00 +0800

    Update c.py

--- c.py
+++ c.py
@@ -2,6 +2,8 @@
 UUID=${UUID:-'xxxxxxxxxxxx'}
 VMPATH=${VMPATH:-'xxxxxxxxxx0'}
 VLPATH=${VLPATH:-'xxxxxxxxxxxxx0'}
+# 不需要隧道去掉下面1个
 TOK=${TOK:-'eyJhIjoiNTRxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxRNCJ9'}
-NEZHA_SERVER='xxxxxxxxxxxx'
+# 不需要哪吒去掉下面2个 
+NEZHA_SERVER='xxxxxxxxxxxx'  
 NEZHA_KEY='xxxxxxxxx'
