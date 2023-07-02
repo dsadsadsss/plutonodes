f1b0bbecc91b78281c868b90f73a0ef2e74c8604
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-07-02 20:16:15 +0800

    Update c.py

--- c.py
+++ c.py
@@ -1,11 +1,11 @@
 #!/bin/bash
-# X-ray设置
+# X-ray设置，可以不修改
 UUID='f0177922-2dcc-4c0f-819c-7b74b7bbbfac'
 VMPATH='vm123456'
 VLPATH='vl123456'
-# A-ro隧道设置
+# A-ro隧道设置，必须填
 TOK='eyJhIxxxxxxxxxxxxxxxxCJ9'}
-# 哪吒设置
+# 哪吒设置，不需要就删掉
 NEZHA_SERVER='xxxxxxg'
 NEZHA_KEY='MxxxxxxxxxI'
 # =====================以下内容都不建议修改=============
