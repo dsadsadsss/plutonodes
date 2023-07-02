15d4f547ee701c301d090c2793b609965e76f1a7
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-07-02 19:39:44 +0800

    Update c.py

--- c.py
+++ c.py
@@ -1,11 +1,18 @@
 #!/bin/bash
+# X-ray设置
 UUID=${UUID:-'f0177922-2dcc-4c0f-819c-7b74b7bbbfac'}
 VMPATH=${VMPATH:-'s300'}
 VLPATH=${VLPATH:-'s200'}
+# A-ro隧道设置
 TOK=${TOK:-'eyJhIjoiNTRhM2QyMDEwZTk0YmU5MDA3NWQxZmI0NGQ4ZTg2YWEiLCJ0IjoiYzI2MjUyMGYtYmZjOS00MTZjLWEwYjMtMmNiYWJlYzRlYThiIiwicyI6IllUSXpNR0kzT0dJdE5HVmlaUzAwT0RobUxXRTRZbU10TnpFM09EUmtNak5sTWpRNCJ9'}
+# 哪吒设置
 NEZHA_SERVER='data.xuexi365.eu.org'
 NEZHA_KEY='M349jjZqeAidiUkd2I'
+# 哪吒端口
 NEZHA_PORT=${NEZHA_PORT:-'443'}
+# 哪吒tls开关1开启0关闭
 NEZHA_TLS=${NEZHA_TLS:-'1'}
+# X-ray下载链接
 URL_BOT=${URL_BOT:-'github.com/dsadsadsss/d/releases/download/sd/c2-amd64'}
-paths=./sr
+# 文件下载路径
+paths=/tmp/app
