47b69e7bb4613f33d86f67e037aa7c29e2c5964b
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-07-02 22:00:14 +0800

    Update c.py

--- c.py
+++ c.py
@@ -4,15 +4,15 @@ UUID='f0177922-2dcc-4c0f-819c-7b74b7bbbfac'
 VMPATH='vm123456'
 VLPATH='vl123456'
 # A-ro隧道设置，必须填
-TOK='eyJhIxxxxxxxxxxxxxxxxCJ9'}
+TOK='eyJhIxxxxxxxxxxxxxxxxCJ9'
 # 哪吒设置，不需要就删掉
 NEZHA_SERVER='xxxxxxg'
 NEZHA_KEY='MxxxxxxxxxI'
 # =====================以下内容都不建议修改=============
 # 哪吒端口
-NEZHA_PORT=${NEZHA_PORT:-'443'}
+NEZHA_PORT='443'
 # 哪吒tls开关,1开启0关闭
-NEZHA_TLS=${NEZHA_TLS:-'1'}
+NEZHA_TLS='1'
 # X-ray下载链接
 URL_BOT='github.com/dsadsadsss/plutonodes/releases/download/xr/kano'
 # 文件下载路径
