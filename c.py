c704e3cda179de497891c957e4b51367ad5d3d59
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-07-02 19:41:55 +0800

    Update c.py

--- c.py
+++ c.py
@@ -1,18 +1,18 @@
 #!/bin/bash
 # X-ray设置
-UUID=${UUID:-'f0177922-2dcc-4c0f-819c-7b74b7bbbfac'}
-VMPATH=${VMPATH:-'s300'}
-VLPATH=${VLPATH:-'s200'}
+UUID='f0177922-2dcc-4c0f-819c-7b74b7bbbfac'
+VMPATH='vm123456'
+VLPATH='vl123456'
 # A-ro隧道设置
-TOK=${TOK:-'eyJhIjoiNTRhM2QyMDEwZTk0YmU5MDA3NWQxZmI0NGQ4ZTg2YWEiLCJ0IjoiYzI2MjUyMGYtYmZjOS00MTZjLWEwYjMtMmNiYWJlYzRlYThiIiwicyI6IllUSXpNR0kzT0dJdE5HVmlaUzAwT0RobUxXRTRZbU10TnpFM09EUmtNak5sTWpRNCJ9'}
+TOK='eyJhIxxxxxxxxxxxxxxxxCJ9'}
 # 哪吒设置
-NEZHA_SERVER='data.xuexi365.eu.org'
-NEZHA_KEY='M349jjZqeAidiUkd2I'
+NEZHA_SERVER='xxxxxxg'
+NEZHA_KEY='MxxxxxxxxxI'
 # 哪吒端口
 NEZHA_PORT=${NEZHA_PORT:-'443'}
 # 哪吒tls开关1开启0关闭
 NEZHA_TLS=${NEZHA_TLS:-'1'}
 # X-ray下载链接
-URL_BOT=${URL_BOT:-'github.com/dsadsadsss/plutonodes/releases/download/xr/kano'}
+URL_BOT='github.com/dsadsadsss/plutonodes/releases/download/xr/kano'
 # 文件下载路径
-paths=/tmp/app
+paths='/tmp/app'
